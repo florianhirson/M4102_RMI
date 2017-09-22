1a55661e273fb3f20eb7980f2e96a64a0e1bb1f2
Florian HIRSON <florian.hirson@etudiant.univ-lille1.fr>
2017-09-22 17:04:39 +0200

    Update

--- src/fr/univ_lille/iut/m4102/TalonCalculatrice.java
+++ src/fr/univ_lille/iut/m4102/TalonCalculatrice.java
@@ -1,98 +1,75 @@
 package fr.univ_lille.iut.m4102;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TalonCalculatrice implements Runnable{
-	private Socket client;
-	Calculatrice action = new Calculatrice();
-
-	public TalonCalculatrice(Socket c){
-		client=c;
+	
+	Socket unClient;
+	
+	public TalonCalculatrice(Socket unClient) {
+		this.unClient = unClient;
+		this.run();
 	}
-
+	
 	@Override
 	public void run() {
 		PrintWriter envoi = null;
 		BufferedReader reception = null;
-		String op="";
-		String tmp="";
-		String a="",b="";
-		int i=0;
-		
 		try {
-			envoi = new PrintWriter(client.getOutputStream(), true );
-		
-
-			reception = new BufferedReader( new
-					InputStreamReader(client.getInputStream()));
+	
+			envoi = new PrintWriter(unClient.getOutputStream(), true);
 			
-			while(!(tmp = reception.readLine()).equals("=")){
-				switch(i){
-				case 0:
-					a=String.valueOf(tmp);
-					break;
-				case 1:
-					op=String.valueOf(tmp);
-					break;
-				case 2:
-					b=String.valueOf(tmp);
-					break;
-				default :
-					break;
+			reception = new BufferedReader(new InputStreamReader(unClient.getInputStream()));
+			
+			Calculatrice cal = new Calculatrice();
+			String line = reception.readLine();
+			String in1="";
+			String in2="";
+			String op="";
+			
+			for(int i=0; i<line.length();i++){
+				if(op.equals("")){
+					if(line.charAt(i)=='.' ||line.charAt(i)=='0' || line.charAt(i)=='1' || line.charAt(i)=='2' || line.charAt(i)=='3' || line.charAt(i)=='4' || line.charAt(i)=='5' || line.charAt(i)=='6' || line.charAt(i)=='7' || line.charAt(i)=='8' || line.charAt(i)=='9'){
+						in1=in1+line.charAt(i);
+					}
+				}else {
+					if(line.charAt(i)=='.' ||line.charAt(i)=='0' || line.charAt(i)=='1' || line.charAt(i)=='2' || line.charAt(i)=='3' || line.charAt(i)=='4' || line.charAt(i)=='5' || line.charAt(i)=='6' || line.charAt(i)=='7' || line.charAt(i)=='8' || line.charAt(i)=='9'){
+						in2=in2+line.charAt(i);
+					}
 				}
 				
-				i++;
+				if(line.charAt(i)=='*' ||line.charAt(i)=='-'||line.charAt(i)=='+'||line.charAt(i)=='/'||line.charAt(i)=='%'){
+					op=op+line.charAt(i);
+				}
 			}
 			
+			//verification
+			if(op.length()>1 || in1.lastIndexOf('.')!=in1.indexOf('.') || in2.lastIndexOf('.')!=in2.indexOf('.')){
+				String message = "Le format entré n'est pas valide (FORMAT: XX.X+X.XX) \n  "+ in1 +" / "+op+" / "+in2;
+				envoi.println(message);
+			}else if(op.charAt(0)=='/' && Double.parseDouble(in2)==0.0){
+				String message = "Impossible de diviser par 0.";
+				envoi.println(message);
+			}else{
+				cal.setNb1(Double.parseDouble(in1));
+				cal.setNb2(Double.parseDouble(in2));
+				cal.setOp(op.charAt(0));
+				cal.calcul();
+			
+				String message = "Result of : "+cal.getNb1() +" "+ cal.getOp() +" "+ cal.getNb2() + " = "+cal.getOut();
+				envoi.println(message);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 		
-
-		envoi.println(a+" "+op+" "+b+" = "+traitement_ext(a,b,op)+"");
-		reception.close();
-		envoi.close();
-		} catch (IOException | DivisionParZero e1) {
-			e1.printStackTrace();
-		}
-
 	}
-
-	private double traitement_ext(String a, String b, String op) throws DivisionParZero, FileNotFoundException{
-		double resultat;
-		switch(op.charAt(0)){
-		case '+':
-			resultat = action.addition(Double.parseDouble(a),Double.parseDouble(b) );
-			break;
-			
-		case '*':
-			resultat = action.multiplication(Double.parseDouble(a),Double.parseDouble(b));
-			break;
-			
-		case '/': 
-			try {
-				resultat =action.division(Double.parseDouble(a),Double.parseDouble(b));
-			} catch (NumberFormatException e1) {
-				throw new NumberFormatException("Indiquez un nombre !");
-			} catch (DivisionParZero e1) {
-				throw new DivisionParZero("Division par zero");
-			}
-			break;
-
-		case '-': 
-			resultat = action.soustraction(Double.parseDouble(a),Double.parseDouble(b));
-			break;
-
-		default: 
-			throw new NullPointerException("Veuillez indiquer une operande : +-*/");
-
-		}
-		return resultat;
-	}
-
 
 }
-
 
