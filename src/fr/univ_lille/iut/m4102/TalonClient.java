1292d0d9630deb4d3c19593a8c53b5edd70e1213
Florian HIRSON <florian.hirson@etudiant.univ-lille1.fr>
2017-09-22 17:09:37 +0200

    Update

--- src/fr/univ_lille/iut/m4102/TalonClient.java
+++ src/fr/univ_lille/iut/m4102/TalonClient.java
@@ -2,8 +2,10 @@ package fr.univ_lille.iut.m4102;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class TalonClient {
 	private String in;
@@ -16,8 +18,29 @@ public class TalonClient {
 		this.clientSocket = clientSocket;
 		this.envoi = envoi;
 		this.reception = reception;
-		this.calcul();
 	}
+	
+	public TalonClient(String in,String host, int port) {
+		this.in =in;
+		try {
+		    clientSocket = new Socket(host, port);
+		} catch (UnknownHostException e) {
+		    e.printStackTrace();
+		    System.exit(1);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		    System.exit(1);
+		}
+		
+		    try {
+				envoi = new PrintWriter(clientSocket.getOutputStream(), true);
+				reception = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		    
+		}
     
 	public String envoyer(String message) {
 		    envoi.println(message);
