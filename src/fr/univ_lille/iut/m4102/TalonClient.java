2d1d5e3b0a147759d41e15dafcd30ed38cd746c3
Florian HIRSON <florian.hirson@etudiant.univ-lille1.fr>
2017-09-22 17:08:10 +0200

    Update TalonClient.java

--- src/fr/univ_lille/iut/m4102/TalonClient.java
+++ src/fr/univ_lille/iut/m4102/TalonClient.java
@@ -1,9 +1,40 @@
 package fr.univ_lille.iut.m4102;
 
-public class TalonClient {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-	public TalonClient() {
-		// TODO Auto-generated constructor stub
+public class TalonClient {
+	private String in;
+	private Socket clientSocket = null;
+    private PrintWriter envoi = null;
+    private BufferedReader reception = null;
+    
+	public TalonClient(String in, Socket clientSocket, PrintWriter envoi, BufferedReader reception) {
+		this.in = in;
+		this.clientSocket = clientSocket;
+		this.envoi = envoi;
+		this.reception = reception;
+		this.calcul();
 	}
+    
+	public String envoyer(String message) {
+		    envoi.println(message);
+			    
+			    try {
+					return reception.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			    return null;
+	}
+	
+	public void calcul(){
+		String reponse = envoyer(in);
+		System.out.println(reponse);
+	}
+    
 
 }
